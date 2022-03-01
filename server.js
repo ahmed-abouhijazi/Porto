7279c561e09fadffb3caf4c1d56c2b67eef1dbdc
ahmed-abouhijazi <61936147+ahmed-abouhijazi@users.noreply.github.com>
2022-03-01 13:39:15 +0100

    final commit

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express')
-const serverStatic = require('serve-static')
-const path = require('path')
 
 const app = express()
 
-app.use('/',serverStatic(path.join(__dirname,'/dist')))
+app.use(express.static(__dirname+"/dist"))
 
-app.get('.*/',function(req,res){
-    res.sendFile(path.join(__dirname,'/dist/index.html'))
-})
+app.get(/.*/,function(req,res){
+    res.sendfile(__dirname+"/dist/index.html");
+});
 
 const port = process.env.PORT || 8081
 
