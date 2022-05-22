39d502c6d8dae95e9206d8a677db7cee8e8278f9
ahmed-abouhijazi <61936147+ahmed-abouhijazi@users.noreply.github.com>
2022-05-22 22:19:16 +0100

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
-const path = require('path')
+const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+    .BundleAnalyzerPlugin;
 
 module.exports = {
+  configureWebpack: {
+    plugins: [new BundleAnalyzerPlugin()]
+},
   publicPath: process.env.NODE_ENV === 'production'
     ? '/reedp/'
     : '/',
